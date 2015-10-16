55577d1a33f1b732754709f034469d76cdcd4231
albinaurasova <albinaurasova@bk.ru>
2015-10-16 16:59:13 +0300

    push

--- js/main.js
+++ js/main.js
@@ -41,39 +41,39 @@ $(document).ready(function(){
     }
     $.fn.placeholder();
     
-	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
- //    var myOptions = {
- //        zoom: 16,
- //        center: myPlace,
- //        mapTypeId: google.maps.MapTypeId.ROADMAP,
- //        disableDefaultUI: true,
- //        scrollwheel: false,
- //        zoomControl: true
- //    }
- //    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+   var myPlace = new google.maps.LatLng(56.881891, 60.5732520);
+     var myOptions = {
+         zoom: 16,
+         center: myPlace,
+         mapTypeId: google.maps.MapTypeId.ROADMAP,
+         disableDefaultUI: true,
+         scrollwheel: false,
+         zoomControl: true
+     }
+     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
 
- //    var marker = new google.maps.Marker({
-	//     position: myPlace,
-	//     map: map,
-	//     title: "Ярмарка вакансий и стажировок"
-	// });
+     var marker = new google.maps.Marker({
+     position: myPlace,
+	     map: map,
+	     title: "Эридан"
+	 });
 
-    //  var options = {
-    //     $AutoPlay: true,                                
-    //     $SlideDuration: 500,                            
+      var options = {
+         $AutoPlay: true,                                
+         $SlideDuration: 500,                            
 
-    //     $BulletNavigatorOptions: {                      
-    //         $Class: $JssorBulletNavigator$,             
-    //         $ChanceToShow: 2,                           
-    //         $AutoCenter: 1,                            
-    //         $Steps: 1,                                  
-    //         $Lanes: 1,                                  
-    //         $SpacingX: 10,                              
-    //         $SpacingY: 10,                              
-    //         $Orientation: 1                             
-    //     }
-    // };
+         $BulletNavigatorOptions: {                      
+             $Class: $JssorBulletNavigator$,             
+             $ChanceToShow: 2,                           
+             $AutoCenter: 1,                            
+             $Steps: 1,                                  
+             $Lanes: 1,                                  
+             $SpacingX: 10,                              
+             $SpacingY: 10,                              
+             $Orientation: 1                             
+         }
+     };
 
-    // var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+     var jssor_slider1 = new $JssorSlider$("slider1_container", options);
 
 });
